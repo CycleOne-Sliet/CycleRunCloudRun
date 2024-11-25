717db4ccafd913ae1bd10985976a65a2787e451a
Sandeep Kumar <sandeepqmar05@gmail.com>
2024-11-25 13:09:46 +0530

    Dummy push

--- main.go
+++ main.go
@@ -66,6 +66,7 @@ func main() {
 		log.Printf("Defaulting to port %s", port)
 	}
 	log.Printf("Listening on port %s", port)
+	log.Printf("Running now")
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
